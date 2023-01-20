7a0f1628453ac30c35575fac34ff84d0ddc485ff
Lalatendu07 <lalatendu.4391@gmail.com>
2023-01-20 20:46:52 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements():...
+REQUIREMENT_FILE = 'requirements.txt'
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE) as requirement_file :
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace('\n','') for requirement_name in requirement_list]
+        
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
 
 setup(
     name = 'forest_cover_type',
